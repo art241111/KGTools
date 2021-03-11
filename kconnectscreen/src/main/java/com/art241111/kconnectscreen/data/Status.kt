a1f2492922be4f2055aae19404a8a41aa6712dc5
art241111 <artem241120@gmail.com>
2021-03-11 13:50:38 +0300

    Initial commit. Add images

---
+++ kconnectscreen/src/main/java/com/art241111/kconnectscreen/data/Status.kt
@@ -0,0 +1,14 @@
+package com.art241111.kconnectscreen.data
+
+/**
+ * ENUM class that describes
+ * the possible connection status.
+ *
+ * @author Artem Gerasimov.
+ */
+enum class Status {
+    DISCONNECTED,
+    CONNECTING,
+    COMPLETED,
+    ERROR
+}
