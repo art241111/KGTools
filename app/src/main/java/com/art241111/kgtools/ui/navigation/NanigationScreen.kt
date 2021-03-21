7aa5489d10a51acecdb90cef6eb52e6394a20f57
art241111 <art241111@mail.ru>
2021-03-21 12:40:46 +0300

    Add documentation and optimization in NavigationScreen.kt

--- app/src/main/java/com/art241111/kgtools/ui/navigation/NanigationScreen.kt
+++ app/src/main/java/com/art241111/kgtools/ui/navigation/NanigationScreen.kt
@@ -11,17 +11,22 @@ import com.art241111.kconnectscreen.ui.KConnectScreen
 import com.art241111.kcontrolsystem.ui.utils.TiltControl
 import com.art241111.kgtools.ui.RobotVM
 import com.art241111.kgtools.ui.commands.NavigateProgramsScreen
-import com.art241111.kgtools.ui.views.DefaultScreenBody
 import com.art241111.kgtools.ui.mainScreen.MainScreenNavigationVM
 import com.art241111.kgtools.ui.mainScreen.ProgramAndPointsVM
 import com.art241111.kgtools.ui.mainScreen.ProgramAndPointsWithBottomBar
 import com.art241111.kgtools.ui.mainScreen.SurfaceWithProgramAndPoints
 import com.art241111.kgtools.ui.points.AddPoint
 import com.art241111.kgtools.ui.settingScreen.SettingScreen
+import com.art241111.kgtools.ui.views.DefaultScreenBody
 import com.art241111.saveandloadinformation.sharedPreferences.SharedPreferencesHelperForString
 
 const val CONST_IP_NAME = "LOAD_IP"
 
+/**
+ * The screen where the main navigation of the application takes place.
+ *
+ * @author Created by Artem Gerasimov (gerasimov.av.dev@gmail.com).
+ */
 @Composable
 fun MainNavigateScreen(
     navigate: NavigationViewModel,
@@ -36,11 +41,13 @@ fun MainNavigateScreen(
     mainNavigationVM.setNavigation(navigate)
 
     val programAndPointsVM = viewModel<ProgramAndPointsVM>()
-    programAndPointsVM.context = LocalContext.current
-    programAndPointsVM.updateProgram = { mainNavigationVM.updateProgram() }
-    programAndPointsVM.updatePoint = { mainNavigationVM.moveAddPoint() }
-    programAndPointsVM.updateCommand = { mainNavigationVM.moveProgram() }
-    programAndPointsVM.loadPrograms()
+    with(programAndPointsVM) {
+        context = LocalContext.current
+        updateProgram = { mainNavigationVM.updateProgram() }
+        updatePoint = { mainNavigationVM.moveAddPoint() }
+        updateCommand = { mainNavigationVM.moveProgram() }
+        loadPrograms()
+    }
 
     val state = mainNavigationVM.getState()
     val homeState = viewModel<MainScreenNavigationVM>().homeState
@@ -104,7 +111,7 @@ fun MainNavigateScreen(
         }
 
         ShowScreen.CONNECT -> {
-            val ip = sharedPreferences.load(CONST_IP_NAME, "192.168.31.63")
+            val defaultIp = sharedPreferences.load(CONST_IP_NAME, "192.168.31.63")
             KConnectScreen(
                 onBack = { mainNavigationVM.moveHome() },
                 onConnect = { ip -> robot.robot.connect(ip) },
@@ -112,7 +119,7 @@ fun MainNavigateScreen(
                 onIpChange = { newIp ->
                     sharedPreferences.save(CONST_IP_NAME, newIp)
                 },
-                defaultIP = ip
+                defaultIP = defaultIp
             )
         }
     }
