2483389732e737c0bd173500278d02826479f0fb
art241111 <art241111@mail.ru>
2021-04-02 11:49:11 +0300

    Add documentation at Signal.kt

--- kControlLibrary/src/main/java/com/github/poluka/kControlLibrary/actions/service/signal/Signal.kt
+++ kControlLibrary/src/main/java/com/github/poluka/kControlLibrary/actions/service/signal/Signal.kt
@@ -6,6 +6,12 @@ import com.github.poluka.kControlLibrary.dsl.Program
 
 private const val command = "SERVICE;SIGNAL"
 
+/**
+ * This command sends a signal to the robot.
+ * @param signal - the signal that is sent to the robot.
+ *
+ * @author Created by Artem Gerasimov (gerasimov.av.dev@gmail.com).
+ */
 @ExecutedOnTheRobot
 class Signal(private val signal: Int) : Command {
     override fun run(): String = "$command;$signal"
