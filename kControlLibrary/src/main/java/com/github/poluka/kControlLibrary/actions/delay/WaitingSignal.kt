8a21f97a284a340be0ae5a37abc645097710996e
art241111 <art241111@mail.ru>
2021-04-02 11:50:32 +0300

    Add documentation at WaitingSignal.kt.kt

--- kControlLibrary/src/main/java/com/github/poluka/kControlLibrary/actions/delay/WaitingSignal.kt
+++ kControlLibrary/src/main/java/com/github/poluka/kControlLibrary/actions/delay/WaitingSignal.kt
@@ -4,13 +4,16 @@ import com.github.poluka.kControlLibrary.actions.Command
 import com.github.poluka.kControlLibrary.actions.annotation.ExecutedOnTheRobot
 import com.github.poluka.kControlLibrary.dsl.Program
 
-/**
- *
- */
 private const val command = "SERVICE;WSIGNAL"
 
+/**
+ * This command wait, when the robot get this signal.
+ * @param signal - the signal that the robot is waiting for.
+ *
+ * @author Created by Artem Gerasimov (gerasimov.av.dev@gmail.com).
+ */
 @ExecutedOnTheRobot
-class WaitingSignal(val signal: Int) : Command {
+class WaitingSignal(private val signal: Int) : Command {
     override fun run(): String = "$command;$signal"
 }
 
